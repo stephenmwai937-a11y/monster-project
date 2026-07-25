b8a28066fb39204c4eee3efa788a72bdda142298
stephenmwai937-a11y <stephenmwai937@gmail.com>
2026-07-25 16:53:29 +0300

    Add JavaScript file with welcome message

---
+++ script.js
@@ -0,0 +1,11 @@
+// Welcome Message
+alert("Welcome to Monster Energy!");
+
+let userName = prompt("What is your name?");
+
+console.log("Welcome " + userName + "!");
+
+// Display a greeting in the browser console
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Monster Energy website loaded successfully.");
+});
